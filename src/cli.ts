97ee4fc76b0281a996b4c8fee92c5a1fa4711f6f
stsysd <000stsysd000@gmail.com>
2021-12-05 22:10:26 +0900

    :sparkles: implement ADD, LIST, and DONE

---
+++ src/cli.ts
@@ -0,0 +1,195 @@
+import { Input } from "https://deno.land/x/cliffy@v0.20.1/prompt/mod.ts";
+import { readLines } from "https://deno.land/std@0.115.1/io/mod.ts";
+import { Todo } from "./todo.ts";
+import {
+  Command,
+  Cmd,
+  Opt,
+  Rest,
+  Arg,
+  Help,
+  Name,
+} from "https://raw.githubusercontent.com/stsysd/classopt/main/mod.ts";
+
+type Context = { path: string };
+
+@Help("Add Todo")
+class Add extends Command<Context> {
+  @Arg({ optional: true })
+  description = "";
+
+  @Opt({ long: false, short: "A" })
+  priorityA = false;
+
+  @Opt({ long: false, short: "B" })
+  priorityB = false;
+
+  @Opt({ long: false, short: "C" })
+  priorityC = false;
+
+  @Opt({ long: false, short: "D" })
+  priorityD = false;
+
+  @Opt({ type: "string", about: "priority" })
+  priority = "";
+
+  get priority_(): string | undefined {
+    if (this.priority) {
+      const p = this.priority[0];
+      if (/[A-Z]/.test(p)) {
+        throw `invalid option valid: --priority ${this.priority}`;
+      }
+      return this.priority[0];
+    }
+    if (this.priorityA) return "A";
+    if (this.priorityB) return "B";
+    if (this.priorityC) return "C";
+    if (this.priorityD) return "D";
+    return undefined;
+  }
+
+  async execute(ctxt: Context) {
+    let todo;
+    if (this.description) {
+      todo = new Todo(this.description, {
+        creationDate: new Date(),
+        priority: this.priority_,
+      });
+    } else {
+      todo = await this.interactive();
+    }
+    Todo.append(ctxt.path, todo);
+    console.log("Add Todo:");
+    console.log(todo.serialize({ color: true }));
+  }
+
+  async interactive(): Promise<Todo> {
+    const priority = await Input.prompt({
+      message: "Priority",
+      hint: "A-Z",
+      validate(c) {
+        return c === "" || /^[A-Z]$/.test(c);
+      },
+    });
+    const description = await Input.prompt({
+      message: "Description",
+    });
+    const creationDate = new Date();
+    return new Todo(description, { priority, creationDate });
+  }
+}
+
+// deno-lint-ignore no-explicit-any
+function compare(l: any, r: any): number {
+  if (l < r) return -1;
+  if (l > r) return 1;
+  return 0;
+}
+
+@Help("List Todo")
+class List extends Command<Context> {
+  @Opt({ type: "string", multiple: true })
+  filter!: string[];
+
+  @Opt({ about: "print todos with index number" })
+  index = false;
+
+  @Opt({ short: "A", about: "print todos including completed" })
+  all = false;
+
+  async execute(ctxt: Context) {
+    try {
+      const todos = await Todo.load(ctxt.path);
+      let todosWithIndex = [...todos.entries()];
+      if (!this.all) {
+        todosWithIndex = todosWithIndex.filter(([_, todo]) => !todo.completion);
+      }
+      if (this.filter.length > 0) {
+        todosWithIndex = todosWithIndex.filter(([_, todo]) =>
+          this.filter.some((f) => todo.description.includes(f))
+        );
+      }
+      todosWithIndex.sort(
+        ([_i, l], [_j, r]) =>
+          compare(l.completion, r.completion) || compare(l.priority, r.priority)
+      );
+      if (!this.index) {
+        todosWithIndex.forEach(([_, todo]) =>
+          console.log(todo.serialize({ color: true, align: true }))
+        );
+      } else {
+        const len = Math.floor(Math.log10(todos.length));
+        todosWithIndex.forEach(([i, todo]) => {
+          const idx = `${i}`.padStart(len, "0");
+          console.log(
+            `${idx}: ` + todo.serialize({ color: true, align: true })
+          );
+        });
+      }
+    } catch (e) {
+      if (e instanceof Deno.errors.NotFound) {
+        console.info(e.message);
+        Deno.exit(1);
+      }
+      throw e;
+    }
+  }
+}
+
+@Help("Complete Todo")
+@Name("done")
+class Complete extends Command<Context> {
+  @Rest()
+  indexes: string[] = [];
+
+  async execute(ctxt: Context) {
+    const todos = await Todo.load(ctxt.path);
+    if (this.indexes.length) {
+      for (const line of this.indexes) {
+        const ix = parseInt(line);
+        if (Number.isNaN(ix)) continue;
+        const todo = todos[ix];
+        if (todo == null) continue;
+        todo.completion = true;
+        todo.completionDate = new Date();
+      }
+    } else {
+      for await (const line of readLines(Deno.stdin)) {
+        const ix = parseInt(line);
+        if (Number.isNaN(ix)) continue;
+        const todo = todos[ix];
+        if (todo == null) continue;
+        todo.completion = true;
+        todo.completionDate = new Date();
+      }
+    }
+    await Todo.save(ctxt.path, todos);
+  }
+}
+
+function homeDir(): string {
+  return Deno.env.get("HOME") ?? Deno.env.get("USERPROFILE") ?? "";
+}
+
+@Name("Todo")
+@Help("Todo.txt Manager")
+class Root extends Command {
+  @Cmd(Add, List, Complete)
+  command?: Command<Context>;
+
+  @Opt({ type: "string", short: "P", about: "Path to file" })
+  dir = homeDir();
+
+  get path(): string {
+    return `${this.dir || "."}/.todo.txt`;
+  }
+
+  async execute() {
+    if (this.command) {
+      return await this.command.execute(this);
+    }
+    console.log(this.help());
+  }
+}
+
+await Root.run(Deno.args);
---
+++ src/todo.ts
@@ -0,0 +1,170 @@
+import { format, parse } from "https://deno.land/std@0.115.1/datetime/mod.ts";
+import {
+  green,
+  red,
+  yellow,
+  dim,
+  underline,
+} from "https://deno.land/std@0.115.1/fmt/colors.ts";
+
+const DATE_FMT = "yyyy-MM-dd";
+
+const TODO_PAT =
+  /^(?:(?<completion>x| )\s+)?(?:\((?<priority>[A-Z])\)\s+)?(?:(?<completionDate>\d\d\d\d-\d\d-\d\d)\s+)?(?:(?<creationDate>\d\d\d\d-\d\d-\d\d)\s+)?(?<description>.+)$/;
+const PROJECT_PAT = /\+(?<tag>\S+)/g;
+const CONTEXT_PAT = /\@(?<tag>\S+)/g;
+const METADATA_PAT = /(?<key>\S+):(?<val>\S+)/g;
+
+export class Todo {
+  readonly description: string;
+  completion = false;
+
+  priority?: string;
+  completionDate?: Date;
+  creationDate?: Date;
+
+  constructor(
+    description: string,
+    opts: Partial<{
+      completion: boolean;
+      priority: string;
+      completionDate: Date;
+      creationDate: Date;
+    }> = {}
+  ) {
+    this.description = description;
+    for (const [k, v] of Object.entries(opts)) {
+      // deno-lint-ignore no-explicit-any
+      (this as any)[k] = v;
+    }
+  }
+
+  get projects(): string[] {
+    const projects = [...this.description.matchAll(PROJECT_PAT)].map(
+      (mat) => mat.groups!.tag
+    );
+    // @ts-ignore: memorize
+    return delete this.projects, (this.projects = projects);
+  }
+
+  get contexts(): string[] {
+    const contexts = [...this.description.matchAll(CONTEXT_PAT)].map(
+      (mat) => mat.groups!.tag
+    );
+    // @ts-ignore: memorize
+    return delete this.contexts, (this.contexts = contexts);
+  }
+
+  get metadata(): Record<string, string> {
+    const metadata = Object.fromEntries(
+      [...this.description.matchAll(METADATA_PAT)].map((mat) => [
+        mat.groups!.key,
+        mat.groups!.val,
+      ])
+    );
+    // @ts-ignore: memorize
+    return delete this.metadata, (this.metadata = metadata);
+  }
+
+  static deserialize(line: string): Todo | null {
+    const mat = TODO_PAT.exec(line);
+    if (mat == null || mat.groups == null) return null;
+    const { completion, priority, completionDate, creationDate } = mat.groups;
+    const description = mat.groups.description ?? "";
+    const todo = new Todo(description ?? "");
+    todo.completion = completion === "x";
+    if (priority) {
+      todo.priority = priority;
+    }
+    if (completionDate) {
+      todo.completionDate = parse(completionDate, DATE_FMT);
+    }
+    if (creationDate) {
+      todo.creationDate = parse(creationDate, DATE_FMT);
+    }
+    if (todo.completionDate == null && todo.creationDate == null) return todo;
+    if (todo.completionDate != null && todo.creationDate != null) return todo;
+    const date: Date = (todo.completionDate ?? todo.creationDate)!;
+    if (todo.completion) {
+      todo.completionDate = date;
+      delete todo.creationDate;
+    } else {
+      todo.creationDate = date;
+      delete todo.completionDate;
+    }
+    return todo;
+  }
+
+  static async load(fname: string): Promise<Todo[]> {
+    const txt = await Deno.readTextFile(fname);
+    return txt
+      .split("\n")
+      .map((line, i) => {
+        if (line === "") return null;
+        const todo = Todo.deserialize(line);
+        if (todo == null) {
+          console.warn(`wrong format at ${i} of '${fname}'`);
+        }
+        return todo;
+      })
+      .filter(Boolean) as Todo[];
+  }
+
+  static async save(fname: string, todos: Todo[]): Promise<void> {
+    await Deno.writeTextFile(
+      fname,
+      todos.map((todo) => todo.serialize() + "\n").join("")
+    );
+  }
+  static async append(fname: string, todo: Todo): Promise<void> {
+    await Deno.writeTextFile(fname, todo.serialize() + "\n", { append: true });
+  }
+
+  serialize(opts: { color?: boolean; align?: boolean } = {}): string {
+    const words = [];
+
+    if (this.completion) {
+      words.push("x");
+    } else if (opts.align) {
+      words.push(" ");
+    }
+
+    if (this.priority) {
+      words.push(`(${this.priority})`);
+    } else if (opts.align) {
+      words.push(" ".repeat(3));
+    }
+
+    if (this.completionDate) {
+      const fmt = format(this.completionDate, DATE_FMT);
+      words.push(opts.color ? underline(fmt) : fmt);
+    } else if (opts.align) {
+      const spaces = " ".repeat(DATE_FMT.length);
+      words.push(opts.color ? underline(spaces) : spaces);
+    }
+
+    if (this.creationDate) {
+      const fmt = format(this.creationDate, DATE_FMT);
+      words.push(opts.color ? underline(fmt) : fmt);
+    } else if (opts.align) {
+      const spaces = " ".repeat(DATE_FMT.length);
+      words.push(opts.color ? underline(spaces) : spaces);
+    }
+
+    if (opts.color) {
+      let description = this.description;
+      description = description.replaceAll(PROJECT_PAT, red);
+      description = description.replaceAll(CONTEXT_PAT, yellow);
+      description = description.replaceAll(METADATA_PAT, green);
+      words.push(description);
+    } else {
+      words.push(this.description);
+    }
+
+    let str = words.join(" ");
+    if (opts.color && this.completion) {
+      str = dim(str);
+    }
+    return str;
+  }
+}
